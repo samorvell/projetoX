f4a22a7ba033b878c34b5e57f91282d6d13098f7
samorvell <samuel_amaro_4@hotmail.com>
2021-11-08 22:52:19 -0300

    atualização heroku

--- src/test/java/com/samorvell/pontointeligente/api/ApplicationTests.java
+++ src/test/java/com/samorvell/pontointeligente/api/ApplicationTests.java
@@ -1,18 +1,12 @@
 package com.samorvell.pontointeligente.api;
 
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@ActiveProfiles("test")
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@ActiveProfiles("test")
 public class ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+//	@Test
+//	public void contextLoads() {
+//	}
 
 }
