cc1c32128ddbe6b952ebd14d96afa3275e007d1c
samorvell <samuel_amaro_4@hotmail.com>
2021-12-14 23:32:25 -0300

    teste pomxml.

--- src/main/java/com/samorvell/pontointeligente/api/Application.java
+++ src/main/java/com/samorvell/pontointeligente/api/Application.java
@@ -2,7 +2,6 @@ package com.samorvell.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication//(exclude = {SecurityAutoConfiguration.class})
